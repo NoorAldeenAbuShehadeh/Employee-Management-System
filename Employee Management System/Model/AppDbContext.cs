c14302ef7103a61a2ae318f357a2474e09fc5321
Noor Aldeen <anooraldeen9@gmail.com>
2023-12-04 16:14:43 +0200

    initialize folders and files configrations

---
+++ Employee Management System/Model/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Management_System.Model
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        public AppDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(_connectionString);
+        }
+    }
+}
---
+++ Employee Management System/Program.cs
@@ -0,0 +1,18 @@
+﻿using Employee_Management_System.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+string connectionSting = configuration.GetConnectionString("DefaultConnection");
+var host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddLogging(se => se.AddLog4Net())
+                .AddSingleton(new AppDbContext(connectionSting));
+            })
+            .Build();
